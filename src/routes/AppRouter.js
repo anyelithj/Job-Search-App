749fff33ffde035268f95e42c8a0235c270bb051
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-05-21 13:05:16 -0500

    add struct

---
+++ src/routes/AppRouter.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+       
+     </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
